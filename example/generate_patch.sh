5f788e6f6c607094e03b53e595c41c930a73b12d
Adrian Iain Lam <adrianiainlam@users.noreply.github.com>
2022-01-02 01:43:46 +0000

    Upgrade to Cubism 4 Release R4. No incompatible API changes.

--- example/generate_patch.sh
+++ example/generate_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p demo_clean
-cp -p -r CubismSdkForNative-4-r.2/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
+cp -p -r CubismSdkForNative-4-r.4/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
 diff -pruN --exclude build ./demo_clean ./demo_dev > ./demo.patch

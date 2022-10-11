bc67426f7a951c1b99c5fa6ba5d25ae0869f7a13
Adrian Iain Lam <adrianiainlam@users.noreply.github.com>
2022-10-11 20:30:34 +0100

    Upgrade to Cubism 4 Release R5.1. No incompatible API changes.

--- example/build.sh
+++ example/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 mkdir -p demo_build
-cp -r CubismSdkForNative-4-r.4/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_build/
+cp -r CubismSdkForNative-4-r.5.1/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_build/
 patch -d demo_build -p2 < demo.patch
 ./demo_build/scripts/make_gcc

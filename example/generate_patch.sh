6fd3638f96242a32c377a4eee3e064f75707698b
Adrian Iain Lam <adrianiainlam@users.noreply.github.com>
2023-05-28 08:44:54 +0100

    Upgrade to Cubism 4 Release R7.

--- example/generate_patch.sh
+++ example/generate_patch.sh
@@ -3,9 +3,9 @@
 mkdir -p demo_clean
 
 if [ "$OSTYPE" = "msys" -o "$OSTYPE" = "cygwin" ]; then
-    cp -p -r CubismSdkForNative-4-r.6.2/Samples/OpenGL/Demo/proj.win.cmake/* ./demo_clean/
+    cp -p -r CubismSdkForNative-4-r.7/Samples/OpenGL/Demo/proj.win.cmake/* ./demo_clean/
     diff -pruN --exclude build ./demo_clean ./demo_dev > ./demo_win.patch
 else
-    cp -p -r CubismSdkForNative-4-r.6.2/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
+    cp -p -r CubismSdkForNative-4-r.7/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
     diff -pruN --exclude build ./demo_clean ./demo_dev > ./demo.patch
 fi
